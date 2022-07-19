88c75a20ab86c2f74ee7682962a4f4371339c230
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-07-19 12:50:43 -0300

    Descomprimidos algunos `.tar.gz` y renombrados algunos archivos

---
+++ Laboratorio/Parcial 1 2022-05-03 (Tema D)/Parcial 1 2022-05-03 (Tema D) kickstrat/flight.c
@@ -0,0 +1,17 @@
+/*
+  @file layover.c
+  @brief Implements flight structure and methods
+*/
+#include <stdlib.h>
+#include "flight.h"
+
+static const int AMOUNT_OF_FLIGHT_VARS = 3;
+
+Flight flight_from_file(FILE* file)
+{
+    Flight flight;
+
+    /* COMPLETAR */
+
+    return flight;
+}
